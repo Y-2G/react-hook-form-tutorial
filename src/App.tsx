a39f05d9faa2412bfee2eef98f50728ca507964d
Yoshifumi Tsuji <JPZ3177@w022581907C48m.local>
2021-03-11 14:10:07 +0900

    finished section5

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,17 @@
 import React from "react";
-import ReactSelect from "react-select";
-import { TextField, Checkbox } from "@material-ui/core";
-import { useForm, Controller, useController } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
-// ========== COMPONENT ========== 
-
-// const App = () => {
-//   const methods = useForm();
-//   const  { handleSubmit, control, reset } = methods;
-//   const onSubmit = (data: any) => console.log(data);
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <Controller as={TextField} name="TextField" control={control} defaultValue="" />
-
-//       <Controller
-//         name="MyCheckbox"
-//         control={control}
-//         defaultValue={false}
-//         rules={{ required: true }}
-//         render={props => 
-//           <Checkbox
-//             onChange={ e => props.onChange(e.target.checked)}
-//             checked={props.value}
-//           />
-//         }
-//       />
-
-//       <input type="submit" />
-//     </form>
-//   )
-// }
-
-// ========== HOOK ========== 
-
-const Input = ({ control, name }: any) => {
-  const {
-    field: { ref, ...inputProps },
-    meta: { invalid, isTouched, isDirty },
-  } = useController({
-    name,
-    control,
-    rules: { required: true },
-    defaultValue: "",
-  });
-
-  return <TextField {...inputProps} inputRef={ref} />
-}
 
 const App = () => {
-  const { handleSubmit, control } = useForm();
+  const { register, errors, handleSubmit } = useForm();
   const onSubmit = (data: any) => console.log(data);
-  
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Input name="firstName" control={control} />
+      <input name="firstName" ref={register({ required: true })} />
+      {errors.firstName && "First name is required"}
+      <input name="lastName"  ref={register({ required: true })} />
+      {errors.lastName && "Last name is required"}
       <input type="submit" />
     </form>
   );
